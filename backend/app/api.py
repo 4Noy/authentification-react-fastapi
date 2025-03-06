0618f5c0736a4f896b74f2ce45931bd049776b5d
noe.dangla <noe.dangla@gmail.com>
2025-03-06 10:18:07 +0100

    feat(backend): add-connect-endpoint

--- backend/app/api.py
+++ backend/app/api.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI, HTTPException, Depends
 from sqlalchemy.orm import Session
-import logging
 
 from app.user import UserDBModel, UserAPIModel
 from app.database import get_db
@@ -9,19 +8,20 @@ app = FastAPI()
 
 
 @app.get("/")
-async def read_root():
+async def read_root() -> dict:
     return {"message": "Connection Established to FastAPI !"}
 
 
 @app.get("/users")
-async def get_users(db: Session = Depends(get_db)):
+async def get_users(db: Session = Depends(get_db)) -> dict:
     return {"users":
             [user_login[0] for user_login in
              db.query(UserDBModel.login).all()]}
 
 
 @app.post("/create_user")
-async def create_user(newuser: UserAPIModel, db: Session = Depends(get_db)):
+async def create_user(newuser: UserAPIModel,
+                      db: Session = Depends(get_db)) -> dict:
     if (any(newuser.login == user_login
             for user_login in db.query(UserDBModel.login))):
         raise HTTPException(status_code=409, detail="User Already Exists")
@@ -30,5 +30,19 @@ async def create_user(newuser: UserAPIModel, db: Session = Depends(get_db)):
         newusermodel = UserDBModel(**newuser.dict())
         db.add(newusermodel)
         db.commit()
+        return {"message": "New User Created"}
     except Exception:
-        logging.warning("Couldn't add new user: " + newuser.login)
+        raise HTTPException(status_code=500, detail="Couln't add new user")
+
+
+@app.post("/connect")
+async def connect(user: UserAPIModel,
+                  token: str,
+                  db: Session = Depends(get_db)) -> dict:
+    connecteduser: UserDBModel or None = db.query(UserDBModel)\
+            .where(UserDBModel.login == user.login).first()
+    if (connecteduser is None):
+        raise HTTPException(status_code=404, detail="User doesn't exist")
+    if (connecteduser.password != user.password):
+        raise HTTPException(status_code=401, detail="Wrong Password !")
+    return {"message": "Connected"}
